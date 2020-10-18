d34e4cbfbf117807e7d8c529c39747a9a2b9e0f0
bolin12 <1569983927@qq.com>
2020-10-18 14:22:24 +0800

    sd

--- demo.py
+++ demo.py
@@ -21,19 +21,24 @@ I = ti.Matrix([
             [0.0, 1.0]
         ])
 a = ti.Vector([1.0,2.0])
-# b = ti.Vector(1,ti.f32, shape=(2))
-# C = ti.Vector(1, dt=ti.f32, shape=2)
 
+A = ti.Matrix([
+    [1.0, 2.0],
+    [3.0, 4.0]
+])
+print("A", A)
+b = ti.Vector([1.0, 2.0])
+print("b", b)
 @ti.kernel
 def demo():
-
-    b = a.outer_product(a)
-    print(b,b.n)
-    print(a.n)
-    print(a)
-    for i in ti.static(range(2)):
-        for j in ti.static(range(2)):
-            print(i,j, "-th component is", b[i,j])
+    c = A @ b
+    print(c,c.n)
+    d = b.dot(b)
+    print(d)
+    e = A*A
+    print(e)
+    f = (A.T() @ A).trace()
+    print(f) 
 
 demo()
 
